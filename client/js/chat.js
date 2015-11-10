59b734f26332ca8268639ec0181d3dc849985945
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-11-10 12:21:32 -0700

    finish io emit

--- client/js/chat.js
+++ client/js/chat.js
@@ -1,3 +1,15 @@
 $(function() {
   var socket = io();
+
+  $('form').submit(function(event) {
+    event.preventDefault();
+    var message = $('#m').val();
+
+    socket.emit('chat message', message);
+    $('#m').val('');
+  });
+
+  socket.on('chat message', function(event) {
+    $('#messages').append($('<li>').text(event));
+  });
 });
--- server/app.js
+++ server/app.js
@@ -9,14 +9,21 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 app.use(express.static(path.join(__dirname, '../client')));
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   res.render('index');
 });
 
-server.listen(3000, function(){
+server.listen(3000, function() {
   console.log('listening on localhost:3000');
 });
 
-io.on('connection', function(socket){
-  console.log("someone entered the chat room!");
+io.on('connection', function(socket) {
+  console.log('A user connected');
+  socket.on('disconnect', function() {
+  console.log('user disconnected');
+  });
+  socket.on('chat message', function(message) {
+    console.log('chat message', message);
+    io.emit('chat message', message);
+  });
 });
